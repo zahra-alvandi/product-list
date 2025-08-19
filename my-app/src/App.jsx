0ce781dd65d0c5300aa5089445e1abba13126cc7
Zahra Alvandi <zalvandi39@gmail.com>
2025-08-19 20:13:35 +0330

    part-1

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Product from './Products/products'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Product></Product>
+    </div>
+  )
+}
+
+export default App
---
+++ my-app/src/Products/products.jsx
@@ -0,0 +1,34 @@
+import "./products.css"
+
+function Product() {
+    return (
+        <div>
+      <div className='header-title'>
+        <h2>Dessert</h2>
+      </div>
+
+      <div>
+        <div>
+          <div className="product-img">
+            <img className="products-img" src="/images/image-waffle-mobile.jpg" alt="" />
+          <div className='adding-btn'>
+            <button className="add-to-cart-btn">
+                <img src="images/icon-add-to-cart.svg" alt="" />
+                Add to Cart
+            </button>
+          </div>
+          </div>
+        </div>
+
+        <div>
+          <span>Waffle</span>
+          <p>Waffle with Berries</p>
+          <p>$6.50</p>
+        </div>
+
+      </div>
+    </div>
+    )
+}
+
+export default Product
